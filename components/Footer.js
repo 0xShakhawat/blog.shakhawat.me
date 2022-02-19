c7934b7cea9d8725b2239edf8dadf963f2fb2459
Shakhawat Hossain <52792157+0xShakhawat@users.noreply.github.com>
2022-02-19 22:50:23 +0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -22,7 +22,9 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
- ​         ​<​Link​ ​href​=​"https://www.github.com/0xShakhawat"​>​ViewGitHubRepository​<​/​Link​>
+ ​         ​<​Link​ ​href​=​"https://www.github.com/0xShakhawat"​>​
+            ViewGitHubRepository​
+          <​/​Link​>
         </div>
       </div>
     </footer>
