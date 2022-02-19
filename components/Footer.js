a7e916d763fac42383ca7e4570ab11e7d063e67b
Shakhawat Hossain <52792157+0xShakhawat@users.noreply.github.com>
2022-02-19 23:38:22 +0600

    Update Footer.js
    Portfolio link added

--- components/Footer.js
+++ components/Footer.js
@@ -15,7 +15,7 @@ export default function Footer() {
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteMetadata.author}</div>
+          ​<​Link​ ​href​=​"https://www.shakhawat.me"​>​Shakhawat​<​/​Link​>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
