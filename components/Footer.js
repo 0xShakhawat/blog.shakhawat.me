14a945ccf5c4754dece11eee6fd65fbb65b626d6
Shakhawat Hossain <52792157+0xShakhawat@users.noreply.github.com>
2022-02-20 09:45:25 +0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -22,9 +22,7 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/0xShakhawat/blog.shakhawat.me">
-            Tailwind Nextjs Them
-          </Link>
+          <Link href="https://github.com/0xShakhawat/blog.shakhawat.me">Tailwind Nextjs Them</Link>
         </div>
       </div>
     </footer>
