3fb392c0a960a0f3c54d3ae3b1ea3cfa89b98cb5
Shakhawat Hossain <52792157+0xShakhawat@users.noreply.github.com>
2022-02-20 12:56:05 +0600

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://blog.shakhawat.me',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/0xShakhawat/blog.shakhawat.me',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
