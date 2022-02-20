96f3ad91cd08d19293137edd4d96ea837684746e
Shakhawat Hossain <52792157+0xShakhawat@users.noreply.github.com>
2022-02-20 10:53:59 +0600

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,7 +2,8 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: 'https://www.shakhawat.me', title: 'About Me' },
+  { href: 'https://linux.shakhawat.me', title: 'হাতেকলমে লিনাক্স' },
 ]
 
 export default headerNavLinks
