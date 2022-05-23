d1c8454d5013dc0e0d19b23933d38bfc806c5fde
Shakhawat Hossain <52792157+0xShakhawat@users.noreply.github.com>
2022-05-23 22:25:49 +0600

    Create NewsletterForm.js

--- components/NewsletterForm.js
+++ components/NewsletterForm.js
@@ -2,7 +2,7 @@ import { useRef, useState } from 'react'
 
 import siteMetadata from '@/data/siteMetadata'
 
-const NewsletterForm = ({ title = 'Subscribe to Stay Updated!' }) => {
+const NewsletterForm = ({ title = 'Subscribe to the newsletter' }) => {
   const inputEl = useRef(null)
   const [error, setError] = useState(false)
   const [message, setMessage] = useState('')
